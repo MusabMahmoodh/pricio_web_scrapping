9c82c4b0bd1bfd696fe0bbca405a7f470f7f0a71
MusabMahmoodh <musabmah70707@gmail.com>
2020-10-28 23:54:11 +0530

    initial UI for cards

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 3000
 // Web scrapping functions
 app.set("views", path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 /***
  * Functions to get data
